549831b46c759fc6c3aac5e29cda8c9da104678b
Borna <45570588+Maya1248@users.noreply.github.com>
2024-05-10 01:56:16 +0200

    Add files via upload

---
+++ quickSort/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define BUFFER_SIZE 64
+
+void print_list(int* list, int length) {
+    for (int i=0; i<length; i++) {
+        printf("%d ", list[i]);
+    }
+    printf("\n");
+}
+
+void shift_swap(int* list, int low, int high) {
+    int tmp = list[high];
+
+    for (int i=high; i>low; i--) {
+        list[i] = list[i-1];
+    }
+    list[low] = tmp;
+}
+
+void quick_sort(int* list, int low, int high, int length) {
+    int pivot_index = low;
+
+    // if only 1 element is left; OR if only 2 elements are left, they are sured to be already sorted from previous iteration
+    if (high - low == 0 || high - low == 1) return;
+
+    for (int i=low; i<high; i++) {
+        if (list[pivot_index] > list[i]) {
+            shift_swap(list, pivot_index, i);
+            pivot_index++;
+        }
+    }
+    
+    //print_list(list,length);
+    quick_sort(list, low, pivot_index, length);  // left of pivot
+    quick_sort(list, pivot_index+1, high, length); // right of pivot
+}
+
+
+int main() {
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+
+    int length = 10000, counter=0;
+    int unsorted_list[10000];
+
+    char buffer[BUFFER_SIZE];
+    int number;
+    FILE* file = fopen("list.txt", "r");
+
+    if (file == NULL) {
+        printf("Failed to open the file.\n");
+        return 1;
+    }
+
+    while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
+        if (sscanf(buffer, "%d", &number) == 1) {
+            unsorted_list[counter] = number;
+            counter++;
+        } else {
+            printf("Error: Non-integer character detected.\n");
+        }
+    }
+
+
+    //print_list(unsorted_list, length);
+    quick_sort(unsorted_list, 0, length, length);
+    print_list(unsorted_list, length);   
+
+    fclose(file);
+
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("\nCPU time used: %f seconds\n", cpu_time_used);
+
+    return 0;
+}
