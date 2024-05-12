9bf783e15591b56fc59d5743e6d6ed62f4085808
Borna <45570588+Maya1248@users.noreply.github.com>
2024-05-12 02:36:28 +0200

    Add files via upload

---
+++ mapping_quicksort_time_complexity/src/C/random_generator.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int generate_number(int low, int high) {
+    time_t seconds;
+    time(&seconds);
+    srand((unsigned) seconds);
+
+    return (rand() % (high - low + 1)) + low;
+}
+
+int generate_list_file(int low, int high, int amount) {
+    FILE* file = fopen("list.txt", "w");
+    if (file == NULL) {
+        return -1;
+    }
+
+    for (int i=0; i<amount; i++) {
+        fprintf(file, "%d\n", generate_number(low, high));
+    }
+    fclose(file);
+
+    return 0;
+}
